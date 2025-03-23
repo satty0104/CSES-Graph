6502f6a8f0a426a3b0d335e26335bd45a91bf4c7
Satyam Bansal <162490036+satty0104@users.noreply.github.com>
2025-03-24 01:41:05 +0530

    Add files via upload
    DSU Based

--- Building_Roads.cpp
+++ Building_Roads.cpp
@@ -1,42 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>>adj(202020);
-vector<int>ans;
-vector<bool>vis(202020);
+vector<int> ans;
 
-
-void dfs(int i){
-    vis[i]=true;
-    for(auto it: adj[i]){
-        if(!vis[it]){
-            dfs(it);
+class DSU {
+public:
+    vector<int> parent, size;
+    
+    DSU(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
         }
     }
-}
 
-int main(){
+    int find(int i) {
+        if (parent[i] == i) return i; 
+        return parent[i] = find(parent[i]); 
+    }
+
+    void Union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) return; 
+        
+        if (size[px] > size[py]) { 
+            parent[py] = px;
+            size[px] += size[py];
+        } else {
+            parent[px] = py;
+            size[py] += size[px]; 
+        }
+    }
+};
+int main() {
     int n, m;
-    cin>>n>>m;
+    cin >> n >> m;
+    DSU dsu(n);
     
-        for(int j=0; j<m; j++){
-            int u, v;
-            cin>>u>>v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-    
-    for(int i=1; i<=n; i++){
-        if(!vis[i]){
+    for (int j = 0; j < m; j++) {
+        int u, v;
+        cin >> u >> v;
+        dsu.Union(u, v);  
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (dsu.find(i) == i) {
             ans.push_back(i);
-            dfs(i);
-            
         }
     }
-    cout<<ans.size()-1<<endl;
-    for(int i=0; i<ans.size()-1; i++){
-     
-        cout<<ans[i]<<" "<<ans[i+1]<<endl;
+
+    cout << ans.size() - 1 << endl;
+    for (int i = 0; i < ans.size() - 1; i++) {
+        cout << ans[i] << " " << ans[i + 1] << endl;
     }
     return 0;
 }
